b7907eeb101c5c27874878908d1554da3d03b5bc
GIAN YAURI <GIAN YAURI@REUZ>
2017-11-01 09:44:56 -0500

    A ver ahora

--- Proyecto_Clinica/src/clases/Cama.java
+++ Proyecto_Clinica/src/clases/Cama.java
@@ -1,16 +1,49 @@
 package clases;
 
 public class Cama {
-	
+	//Campos o atributos
 	private int numeroCama;
 	private double precioDia;
 	private int estado;
 	
+	//Metodo Constructor
 	public Cama(int numeroCama, double precioDia, int estado) {
 		this.numeroCama = numeroCama;
 		this.precioDia = precioDia;
 		this.estado = estado;
 	}
 	
+	//Metodo
+	public String detalleEstado() {
+		switch (estado) {
+			case 0: return "Libre";
+			default: return "Ocupada";
+		}
+	}
+
+	//Metodos Setters & Getters. Propiedades de lectura(get return) y escritura(set void)
+	public int getNumeroCama() {
+		return numeroCama;
+	}
+
+	public void setNumeroCama(int numeroCama) {
+		this.numeroCama = numeroCama;
+	}
+
+	public double getPrecioDia() {
+		return precioDia;
+	}
+
+	public void setPrecioDia(double precioDia) {
+		this.precioDia = precioDia;
+	}
+
+	public int getEstado() {
+		return estado;
+	}
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
 	
 }
--- Proyecto_Clinica/src/clases/Paciente.java
+++ Proyecto_Clinica/src/clases/Paciente.java
@@ -1,10 +1,11 @@
 package clases;
 
 public class Paciente {
-	//
+	//Campos o atributos
 	private int codigoPaciente;
 	private String apellidos, nombres, telefono, dni;
 	
+	//Metodo Constructor
 	public Paciente(int codigoPaciente, String apellidos, String nombres, String telefono, String dni) {
 		this.codigoPaciente = codigoPaciente;
 		this.apellidos = apellidos;
@@ -12,6 +13,46 @@ public class Paciente {
 		this.telefono = telefono;
 		this.dni = dni;
 	}
-	
+
+	//Metodos Setters & Getters. Propiedades de lectura(get return) y escritura(set void)
+	public int getCodigoPaciente() {
+		return codigoPaciente;
+	}
+
+	public void setCodigoPaciente(int codigoPaciente) {
+		this.codigoPaciente = codigoPaciente;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
 	
 }
