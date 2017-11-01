479fcc21ededf57173310837856b0b15ccb8e8e3
GIAN YAURI <GIAN YAURI@REUZ>
2017-11-01 09:41:51 -0500

    Cama, Paciente y Atención, getters and setters

--- Proyecto_Clinica/src/clases/Atencion.java
+++ Proyecto_Clinica/src/clases/Atencion.java
@@ -1,12 +1,12 @@
 package clases;
 
 public class Atencion {
-	//
+	//Campos o atributos
 	private int codigoAtencion, codigoPaciente, fechaAtencion;
 	private double totalPagar;
 	private int estado;
 	
-	
+	//Metodo Constructor
 	public Atencion(int codigoAtencion, int codigoPaciente, int fechaAtencion, double totalPagar, int estado) {
 		this.codigoAtencion = codigoAtencion;
 		this.codigoPaciente = codigoPaciente;
@@ -15,5 +15,53 @@ public class Atencion {
 		this.estado = estado;
 	}
 	
-	
+	//Metodo 
+	public String detalleEstado(){
+		switch (estado) {
+		case 0: return "Pendiente";
+		default:return "Pagado";
+		}
+	}
+
+	//Metodos Setters & Getters. Propiedades de lectura(get return) y escritura(set void)
+	public int getCodigoAtencion() {
+		return codigoAtencion;
+	}
+
+	public void setCodigoAtencion(int codigoAtencion) {
+		this.codigoAtencion = codigoAtencion;
+	}
+
+	public int getCodigoPaciente() {
+		return codigoPaciente;
+	}
+
+	public void setCodigoPaciente(int codigoPaciente) {
+		this.codigoPaciente = codigoPaciente;
+	}
+
+	public int getFechaAtencion() {
+		return fechaAtencion;
+	}
+
+	public void setFechaAtencion(int fechaAtencion) {
+		this.fechaAtencion = fechaAtencion;
+	}
+
+	public double getTotalPagar() {
+		return totalPagar;
+	}
+
+	public void setTotalPagar(double totalPagar) {
+		this.totalPagar = totalPagar;
+	}
+
+	public int getEstado() {
+		return estado;
+	}
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+		
 }
