a6ab4f777fdbc910089d0395443402e37b643677
GIAN YAURI <GIAN YAURI@REUZ>
2017-11-13 08:31:37 -0500

    Solo hice el modelo de la tabla Cama y Arreglo; falta listar en las funciones de CRUD en la tabla. Lo siento

--- Proyecto_Clinica/src/arreglos/ArregloCama.java
+++ Proyecto_Clinica/src/arreglos/ArregloCama.java
@@ -1,5 +1,54 @@
 package arreglos;
+import java.util.ArrayList;
+
+import clases.Cama;
 
 public class ArregloCama {
-
+	private ArrayList<Cama> ca;
+	
+	public ArregloCama(){
+		ca = new ArrayList<>();
+		
+		ca.add(new Cama(1, 15.1, 1));
+		ca.add(new Cama(2, 33.2, 0));
+		ca.add(new Cama(3, 42.3, 0));
+		ca.add(new Cama(4, 53.4, 1));
+		ca.add(new Cama(5, 85.5, 0));
+		ca.add(new Cama(6, 46.6, 1));
+		ca.add(new Cama(7, 37.7, 1));
+		ca.add(new Cama(8, 68.8, 0));
+		ca.add(new Cama(9, 29.9, 1));
+		ca.add(new Cama(10, 18.0, 1));
+	}
+	// Método tamaño
+	public int tamaño(){
+		return ca.size();
+	}
+	// Método obtener
+	public Cama obtener(int posi){
+		return ca.get(posi);
+	}
+	// Método adicionar
+	public void adicionar(Cama obje){
+		ca.add(obje);
+	}
+	// Método eliminarAlFinal
+	public void eliminarFinal(){
+		ca.remove(tamaño()-1);	
+	}
+	// Método eliminarTodo
+	public void eliminarTodo(){
+		ca.clear();
+	}
+	// Método Buscar
+	public Cama buscar(int codigo){
+		for (int i = 0; i < tamaño(); i++) {
+			if(obtener(i).getNumeroCama()==codigo)return obtener(i);
+		}return null;
+	}
+	// Método EliminarPorCod
+	public void eliminarPorCod(Cama obje){
+		ca.remove(obje);
+	}
+	
 }
--- Proyecto_Clinica/src/gui/CamaDLG.java
+++ Proyecto_Clinica/src/gui/CamaDLG.java
@@ -8,7 +8,35 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class CamaDLG extends JDialog {
+import arreglos.ArregloCama;
+import javax.swing.JTable;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+public class CamaDLG extends JDialog implements MouseListener {
+	
+	private ArregloCama ac = new ArregloCama();
+	
+	private JButton btnAdicionar;
+	private JButton btnListar;
+	private JButton btnElimtodo;
+	private JButton btnBuscar;
+	private JButton btnElimxcod;
+	private JButton btnElimfinal;
+	private JTable CamaTabla;
+	private JScrollPane scrollPane;
+	private JLabel lblNumero;
+	private JLabel lblPrecio;
+	private JLabel lblEstado;
+	private JTextField txtNum;
+	private JTextField txtPre;
+	private JComboBox comboBox;
+	private JLabel lblListadoDeCama;
 
 	/**
 	 * Launch the application.
@@ -29,8 +57,85 @@ public class CamaDLG extends JDialog {
 	public CamaDLG() {
 		setTitle("Cama");
 		setModal(true);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 647, 419);
 		getContentPane().setLayout(null);
+		
+		btnAdicionar = new JButton("Adicionar");
+		btnAdicionar.setBounds(10, 22, 89, 44);
+		getContentPane().add(btnAdicionar);
+		
+		btnListar = new JButton("Listar");
+		btnListar.setBounds(114, 22, 89, 44);
+		getContentPane().add(btnListar);
+		
+		btnElimtodo = new JButton("ElimTodo");
+		btnElimtodo.setBounds(532, 22, 89, 44);
+		getContentPane().add(btnElimtodo);
+		
+		btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(220, 22, 89, 44);
+		getContentPane().add(btnBuscar);
+		
+		btnElimxcod = new JButton("ElimXcod");
+		btnElimxcod.setBounds(325, 22, 89, 44);
+		getContentPane().add(btnElimxcod);
+		
+		btnElimfinal = new JButton("ElimFinal");
+		btnElimfinal.setBounds(430, 22, 89, 44);
+		getContentPane().add(btnElimfinal);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(43, 121, 359, 244);
+		getContentPane().add(scrollPane);
+		
+		CamaTabla = new JTable();
+		scrollPane.setViewportView(CamaTabla);
+		
+		lblNumero = new JLabel("Numero");
+		lblNumero.setBounds(442, 124, 46, 14);
+		getContentPane().add(lblNumero);
+		
+		lblPrecio = new JLabel("Precio");
+		lblPrecio.setBounds(442, 164, 46, 14);
+		getContentPane().add(lblPrecio);
+		
+		lblEstado = new JLabel("Estado");
+		lblEstado.setBounds(442, 199, 46, 14);
+		getContentPane().add(lblEstado);
+		
+		txtNum = new JTextField();
+		txtNum.setBounds(498, 121, 86, 20);
+		getContentPane().add(txtNum);
+		txtNum.setColumns(10);
+		
+		txtPre = new JTextField();
+		txtPre.setBounds(498, 161, 86, 20);
+		getContentPane().add(txtPre);
+		txtPre.setColumns(10);
+		
+		comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Libre", "Ocupado"}));
+		comboBox.setBounds(498, 196, 86, 20);
+		getContentPane().add(comboBox);
+		
+		lblListadoDeCama = new JLabel("LISTADO DE CAMA");
+		lblListadoDeCama.setBounds(172, 88, 124, 14);
+		getContentPane().add(lblListadoDeCama);
+		CamaTabla.addMouseListener(this);
 	}
-
+	public void mouseClicked(MouseEvent arg0) {
+		if (arg0.getSource() == CamaTabla) {
+			mouseClickedCamaTabla(arg0);
+		}
+	}
+	public void mouseEntered(MouseEvent arg0) {
+	}
+	public void mouseExited(MouseEvent arg0) {
+	}
+	public void mousePressed(MouseEvent arg0) {
+	}
+	public void mouseReleased(MouseEvent arg0) {
+	}
+	protected void mouseClickedCamaTabla(MouseEvent arg0) {
+	}
 }
